ca40b3bcddc1e422a2693ba7e3cc02366ec81f1a
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2023-03-10 13:00:24 -0300

    refactor: reorder everything

--- addons/gamejolt/gamejolt.gd
+++ addons/gamejolt/gamejolt.gd
@@ -3,27 +3,27 @@ class_name _GameJolt
 
 
 # Signals
-signal batch_completed(response)
-signal data_store_fetch_completed(response)
-signal data_store_get_keys_completed(response)
-signal data_store_remove_completed(response)
-signal data_store_set_completed(response)
-signal data_store_update_completed(response)
-signal friends_completed(response)
-signal scores_add_completed(response)
-signal scores_fetch_completed(response)
-signal scores_get_rank_completed(response)
-signal scores_tables_completed(response)
-signal sessions_check_completed(response)
-signal sessions_close_completed(response)
+signal users_fetch_completed(response)
+signal users_auth_completed(response)
 signal sessions_open_completed(response)
 signal sessions_ping_completed(response)
-signal time_completed(response)
-signal trophies_add_achieved_completed(response)
+signal sessions_check_completed(response)
+signal sessions_close_completed(response)
+signal scores_fetch_completed(response)
+signal scores_tables_completed(response)
+signal scores_add_completed(response)
+signal scores_get_rank_completed(response)
 signal trophies_fetch_completed(response)
+signal trophies_add_achieved_completed(response)
 signal trophies_remove_achieved_completed(response)
-signal users_auth_completed(response)
-signal users_fetch_completed(response)
+signal data_store_set_completed(response)
+signal data_store_update_completed(response)
+signal data_store_remove_completed(response)
+signal data_store_fetch_completed(response)
+signal data_store_get_keys_completed(response)
+signal friends_completed(response)
+signal time_completed(response)
+signal batch_completed(response)
 
 
 # Constants
@@ -98,7 +98,314 @@ func get_user_token() -> String:
 
 
 # Public methods
-# Batch Request
+# Users
+# Returns a user's data.
+func users_fetch(user_name := "", user_ids := []) -> _GameJolt:
+	var data := {
+		"game_id": _game_id,
+	}
+
+	if user_name and user_ids.size():
+		return _dispatch_local_error_response(
+			"users/fetch",
+			MESSAGE_ERROR_DATA_COLLISION + "user_name, user_ids"
+		)
+
+	if user_name:
+		data["username"] = user_name
+
+	elif user_ids.size():
+		for i in user_ids.size():
+			user_ids[i] = str(user_ids[i])
+
+		data["user_id"] = ",".join(user_ids)
+
+	else:
+		data["username"] = _user_name
+
+	return _submit("users/fetch", data)
+
+
+# Authenticates the user's information.
+func users_auth() -> _GameJolt:
+	var data := {
+		"game_id": _game_id,
+		"username": _user_name,
+		"user_token": _user_token,
+	}
+
+	return _submit("users/auth", data)
+
+
+# Sessions
+# Opens a game session for a particular user and allows you to tell Game Jolt that a user is playing your game.
+func sessions_open() -> _GameJolt:
+	var data := {
+		"game_id": _game_id,
+		"username": _user_name,
+		"user_token": _user_token,
+	}
+
+	return _submit("sessions/open", data)
+
+
+# Pings an open session to tell the system that it's still active.
+func sessions_ping(status := "") -> _GameJolt:
+	var data := {
+		"game_id": _game_id,
+		"username": _user_name,
+		"user_token": _user_token,
+	}
+
+	if status:
+		if not status in ["active", "idle"]:
+			return _dispatch_local_error_response(
+				"sessions/ping",
+				"Ping status must be 'active' or 'idle'"
+			)
+
+		data["status"] = status
+
+	return _submit("sessions/ping", data)
+
+
+# Checks to see if there is an open session for the user.
+func sessions_check() -> _GameJolt:
+	var data := {
+		"game_id": _game_id,
+		"username": _user_name,
+		"user_token": _user_token,
+	}
+
+	return _submit("sessions/check", data)
+
+
+# Closes the active session.
+func sessions_close() -> _GameJolt:
+	var data := {
+		"game_id": _game_id,
+		"username": _user_name,
+		"user_token": _user_token,
+	}
+
+	return _submit("sessions/close", data)
+
+
+# Scores
+# Returns a list of scores either for a user or globally for a game.
+func scores_fetch(limit = null, table_id = null, guest := "", better_than = null, worse_than = null, this_user := false) -> _GameJolt:
+	var data := {
+		"game_id": _game_id,
+	}
+
+	var optional_data := {
+		"username": _user_name if this_user else guest if guest else null,
+		"user_token": _user_token if this_user else null,
+		"limit":  limit if limit else null,
+		"table_id":  table_id if table_id else null,
+		"guest": guest if guest else null,
+		"better_than": better_than if better_than else null,
+		"worse_than": worse_than if worse_than else null,
+	}
+
+	if optional_data.get("username") and optional_data.get("guest"):
+		return _dispatch_local_error_response(
+			"scores/fetch",
+			"Parameters 'username' and 'guest' are mutually exclusive"
+		)
+
+	return _submit("scores/fetch", data, optional_data)
+
+
+# Returns a list of high score tables for a game.
+func scores_tables() -> _GameJolt:
+	var data := {
+		"game_id": _game_id,
+	}
+
+	return _submit("scores/tables", data)
+
+
+# Adds a score for a user or guest.
+func scores_add(score: String, sort, table_id = null, guest := "", extra_data = null) -> _GameJolt:
+	var data := {
+		"game_id": _game_id,
+		"score": score,
+		"sort": sort,
+	}
+
+	extra_data = _data_to_string(extra_data)
+
+	var optional_data := {
+		"username": guest if guest else _user_name,
+		"user_token": null if guest else _user_token,
+		"table_id": table_id if table_id else null,
+		"guest": guest if guest else null,
+		"extra_data": extra_data if extra_data else null,
+	}
+
+	return _submit("scores/add", data, optional_data)
+
+
+# Returns the rank of a particular score on a score table.
+func scores_get_rank(sort, table_id := "") -> _GameJolt:
+	var data := {
+		"game_id": _game_id,
+		"sort": sort,
+	}
+
+	var optional_data := {
+		"table_id": table_id if table_id else null,
+	}
+
+	return _submit("scores/get-rank", data, optional_data)
+
+
+# Trophies
+# Returns one trophy or multiple trophies, depending on the parameters passed in.
+func trophies_fetch(achieved = null, trophy_ids := []) -> _GameJolt:
+	var data := {
+		"game_id": _game_id,
+		"username": _user_name,
+		"user_token": _user_token,
+	}
+
+	var optional_data := {
+		"achieved": achieved,
+		"trophy_id": ",".join(trophy_ids) if trophy_ids.size() else null,
+	}
+
+	return _submit("trophies/fetch", data, optional_data)
+
+
+# Sets a trophy as achieved for a particular user.
+func trophies_add_achieved(trophy_id) -> _GameJolt:
+	var data := {
+		"game_id": _game_id,
+		"username": _user_name,
+		"user_token": _user_token,
+		"trophy_id": trophy_id,
+	}
+
+	return _submit("trophies/add-achieved", data)
+
+
+# Remove a previously achieved trophy for a particular user.
+func trophies_remove_achieved(trophy_id) -> _GameJolt:
+	var data := {
+		"game_id": _game_id,
+		"username": _user_name,
+		"user_token": _user_token,
+		"trophy_id": trophy_id,
+	}
+
+	return _submit("trophies/remove-achieved", data)
+
+
+# Data Store
+# Sets data in the data store.
+func data_store_set(key: String, data, global_data := false) -> _GameJolt:
+	data = _data_to_string(data)
+
+	var data_ = {
+		"game_id" : _game_id,
+		"key" : key,
+		"data" : data,
+	}
+
+	var optional_data = {
+		"username" : null if global_data else _user_name,
+		"user_token" : null if global_data else _user_token
+	}
+
+	return _submit("data-store/set", data_, optional_data)
+
+
+# Updates data in the data store.
+func data_store_update(key: String, operation: String, value, global_data := false) -> _GameJolt:
+	var data = {
+		"game_id" : _game_id,
+		"key" : key,
+		"operation" : operation,
+		"value" : value,
+	}
+
+	var optional_data = {
+		"username" : null if global_data else _user_name,
+		"user_token" : null if global_data else _user_token
+	}
+
+	return _submit("data-store/update", data, optional_data)
+
+
+# Removes data from the data store.
+func data_store_remove(key: String, global_data := false) -> _GameJolt:
+	var data = {
+		"game_id" : _game_id,
+		"key" : key,
+	}
+
+	var optional_data = {
+		"username" : null if global_data else _user_name,
+		"user_token" : null if global_data else _user_token
+	}
+
+	return _submit("data-store/remove", data, optional_data)
+
+
+# Returns data from the data store.
+func data_store_fetch(key: String, global_data := false) -> _GameJolt:
+	var data = {
+		"game_id" : _game_id,
+		"key" : key,
+	}
+
+	var optional_data = {
+		"username" : null if global_data else _user_name,
+		"user_token" : null if global_data else _user_token
+	}
+
+	return _submit("data-store/fetch", data, optional_data)
+
+
+# Returns either all the keys in the game's global data store, or all the keys in a user's data store.
+func data_store_get_keys(pattern := "", global_data := false) -> _GameJolt:
+	var data = {
+		"game_id" : _game_id,
+	}
+
+	var optional_data = {
+		"username" : null if global_data else _user_name,
+		"user_token" : null if global_data else _user_token,
+		"pattern": pattern,
+	}
+
+	return _submit("data-store/get-keys", data, optional_data)
+
+
+# Friends
+# Returns the list of a user's friends.
+func friends() -> _GameJolt:
+	var data := {
+		"game_id": _game_id,
+		"username": _user_name,
+		"user_token": _user_token,
+	}
+
+	return _submit("friends", data)
+
+
+# Time
+# Returns the time of the Game Jolt server.
+func time() -> _GameJolt:
+	var data := {
+		"game_id": _game_id,
+	}
+
+	return _submit("time", data)
+
+
+# Batch Calls
 # Perform the batch request after gathering requests with batch_begin and batch end.
 func batch(parallel := false, break_on_error := false) -> _GameJolt:
 	if parallel and break_on_error:
@@ -140,314 +447,14 @@ func batch_end() -> void:
 	_submit_requests = true
 
 
-# Data Store
-# Returns data from the data store.
-func data_store_fetch(key: String, global_data := false) -> _GameJolt:
-	var data = {
-		"game_id" : _game_id,
-		"key" : key,
-	}
-
-	var optional_data = {
-		"username" : null if global_data else _user_name,
-		"user_token" : null if global_data else _user_token
-	}
-
-	return _submit("data-store/fetch", data, optional_data)
-
-
-# Returns either all the keys in the game's global data store, or all the keys in a user's data store.
-func data_store_get_keys(pattern := "", global_data := false) -> _GameJolt:
-	var data = {
-		"game_id" : _game_id,
-	}
-
-	var optional_data = {
-		"username" : null if global_data else _user_name,
-		"user_token" : null if global_data else _user_token,
-		"pattern": pattern,
-	}
-
-	return _submit("data-store/get-keys", data, optional_data)
-
-
-# Removes data from the data store.
-func data_store_remove(key: String, global_data := false) -> _GameJolt:
-	var data = {
-		"game_id" : _game_id,
-		"key" : key,
-	}
-
-	var optional_data = {
-		"username" : null if global_data else _user_name,
-		"user_token" : null if global_data else _user_token
-	}
-
-	return _submit("data-store/remove", data, optional_data)
-
-
-# Sets data in the data store.
-func data_store_set(key: String, data, global_data := false) -> _GameJolt:
-	data = _data_to_string(data)
-
-	var data_ = {
-		"game_id" : _game_id,
-		"key" : key,
-		"data" : data,
-	}
-
-	var optional_data = {
-		"username" : null if global_data else _user_name,
-		"user_token" : null if global_data else _user_token
-	}
-
-	return _submit("data-store/set", data_, optional_data)
-
-
-# Updates data in the data store.
-func data_store_update(key: String, operation: String, value, global_data := false) -> _GameJolt:
-	var data = {
-		"game_id" : _game_id,
-		"key" : key,
-		"operation" : operation,
-		"value" : value,
-	}
-
-	var optional_data = {
-		"username" : null if global_data else _user_name,
-		"user_token" : null if global_data else _user_token
-	}
-
-	return _submit("data-store/update", data, optional_data)
-
-
-# Friends
-# Returns the list of a user's friends.
-func friends() -> _GameJolt:
-	var data := {
-		"game_id": _game_id,
-		"username": _user_name,
-		"user_token": _user_token,
-	}
-
-	return _submit("friends", data)
-
-
-# Scores
-# Adds a score for a user or guest.
-func scores_add(score: String, sort, table_id = null, guest := "", extra_data = null) -> _GameJolt:
-	var data := {
-		"game_id": _game_id,
-		"score": score,
-		"sort": sort,
-	}
-
-	extra_data = _data_to_string(extra_data)
-
-	var optional_data := {
-		"username": guest if guest else _user_name,
-		"user_token": null if guest else _user_token,
-		"table_id": table_id if table_id else null,
-		"guest": guest if guest else null,
-		"extra_data": extra_data if extra_data else null,
-	}
-
-	return _submit("scores/add", data, optional_data)
-
-
-# Returns a list of scores either for a user or globally for a game.
-func scores_fetch(limit = null, table_id = null, guest := "", better_than = null, worse_than = null, this_user := false) -> _GameJolt:
-	var data := {
-		"game_id": _game_id,
-	}
-
-	var optional_data := {
-		"username": _user_name if this_user else guest if guest else null,
-		"user_token": _user_token if this_user else null,
-		"limit":  limit if limit else null,
-		"table_id":  table_id if table_id else null,
-		"guest": guest if guest else null,
-		"better_than": better_than if better_than else null,
-		"worse_than": worse_than if worse_than else null,
-	}
-
-	if optional_data.get("username") and optional_data.get("guest"):
-		return _dispatch_local_error_response(
-			"scores/fetch",
-			"Parameters 'username' and 'guest' are mutually exclusive"
-		)
-
-	return _submit("scores/fetch", data, optional_data)
-
-
-# Returns the rank of a particular score on a score table.
-func scores_get_rank(sort: int, table_id := "") -> _GameJolt:
-	var data := {
-		"game_id": _game_id,
-		"sort": sort,
-	}
-
-	var optional_data := {
-		"table_id": table_id if table_id else null,
-	}
-
-	return _submit("scores/get-rank", data, optional_data)
-
-
-# Returns a list of high score tables for a game.
-func scores_tables() -> _GameJolt:
-	var data := {
-		"game_id": _game_id,
-	}
-
-	return _submit("scores/tables", data)
-
-
-# Sessions
-# Checks to see if there is an open session for the user.
-func sessions_check() -> _GameJolt:
-	var data := {
-		"game_id": _game_id,
-		"username": _user_name,
-		"user_token": _user_token,
-	}
-
-	return _submit("sessions/check", data)
-
-
-# Closes the active session.
-func sessions_close() -> _GameJolt:
-	var data := {
-		"game_id": _game_id,
-		"username": _user_name,
-		"user_token": _user_token,
-	}
-
-	return _submit("sessions/close", data)
-
-
-# Opens a game session for a particular user and allows you to tell Game Jolt that a user is playing your game.
-func sessions_open() -> _GameJolt:
-	var data := {
-		"game_id": _game_id,
-		"username": _user_name,
-		"user_token": _user_token,
-	}
-
-	return _submit("sessions/open", data)
-
-
-# Pings an open session to tell the system that it's still active.
-func sessions_ping(status := "") -> _GameJolt:
-	var data := {
-		"game_id": _game_id,
-		"username": _user_name,
-		"user_token": _user_token,
-	}
-
-	if status:
-		if not status in ["active", "idle"]:
-			return _dispatch_local_error_response(
-				"sessions/ping",
-				"Ping status must be 'active' or 'idle'"
-			)
-
-		data["status"] = status
-
-	return _submit("sessions/ping", data)
-
-
-# Time
-# Returns the time of the Game Jolt server.
-func time() -> _GameJolt:
-	var data := {
-		"game_id": _game_id,
-	}
-
-	return _submit("time", data)
-
-
-# Trophies
-# Returns one trophy or multiple trophies, depending on the parameters passed in.
-func trophies_fetch(achieved = null, trophy_ids := []) -> _GameJolt:
-	var data := {
-		"game_id": _game_id,
-		"username": _user_name,
-		"user_token": _user_token,
-	}
-
-	var optional_data := {
-		"achieved": achieved,
-		"trophy_id": ",".join(trophy_ids) if trophy_ids.size() else null,
-	}
-
-	return _submit("trophies/fetch", data, optional_data)
-
-
-# Sets a trophy as achieved for a particular user.
-func trophies_add_achieved(trophy_id):
-	var data := {
-		"game_id": _game_id,
-		"username": _user_name,
-		"user_token": _user_token,
-		"trophy_id": trophy_id,
-	}
-
-	return _submit("trophies/add-achieved", data)
-
-
-# Remove a previously achieved trophy for a particular user.
-func trophies_remove_achieved(trophy_id):
-	var data := {
-		"game_id": _game_id,
-		"username": _user_name,
-		"user_token": _user_token,
-		"trophy_id": trophy_id,
-	}
-
-	return _submit("trophies/remove-achieved", data)
-
-
-# Users
-# Authenticates the user's information.
-func users_auth() -> _GameJolt:
-	var data := {
-		"game_id": _game_id,
-		"username": _user_name,
-		"user_token": _user_token,
-	}
-
-	return _submit("users/auth", data)
-
-
-# Returns a user's data.
-func users_fetch(_user_name := "", _user_ids := []):
-	var data := {
-		"game_id": _game_id,
-	}
-
-	if _user_name:
-		data["username"] = _user_name
-
-	elif _user_ids.size():
-		for i in _user_ids.size():
-			_user_ids[i] = str(_user_ids[i])
-
-		data["user_id"] = ",".join(_user_ids)
-
-	else:
-		data["username"] = _user_name
-
-	return _submit("users/fetch", data)
-
-
 # Private methods
 # Submit request and return response data on specific signal.
 func _submit(operation: String, data: Dictionary, optional_data := {}) -> _GameJolt:
+	var required_data_error := _validate_required_data(data)
+
 	if optional_data.size():
 		data.merge(_validate_optional_data(optional_data))
 
-	var required_data_error := _validate_required_data(data)
 	var final_url := _generate_url(OPERATIONS[operation], data)
 	if DEBUG: prints(final_url)
 
